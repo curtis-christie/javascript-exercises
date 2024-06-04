83f2f0f9255f7707e16501c3dcf622db1cc60f46
Curtis Christie <103606492+curtis-christie@users.noreply.github.com>
2024-06-04 07:23:18 -0400

    Add repeatString() function. Pass all tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if (num < 0) return "ERROR";
+    let string = "";
+    for (;num>0;num--) {
+        string += word;
+    };
+    return string;
 };
 
 // Do not edit below this line
