f8a301d442d43f272bbcced2c031401feee878aa
Curtis Christie <103606492+curtis-christie@users.noreply.github.com>
2024-06-04 15:48:18 -0400

    complete exercise 3. Add test index.html file.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    const letterArray = [];
+    for (let i=0;i<word.length;i++){
+        letterArray.push(word.charAt(word.length-(i+1)));
+    }
+
+    return(letterArray.join(""));
 
 };
 
