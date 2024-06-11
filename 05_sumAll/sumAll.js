bf5a9ab6a3075192a13e504d4900b10cc20e5a19
Curtis Christie <103606492+curtis-christie@users.noreply.github.com>
2024-06-10 21:07:51 -0400

    solve exercise 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    
+    if (num1 > num2) {
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
 
+    let totalSum = 0;
+
+    for (let i = num1; i <= num2; i++) {
+        totalSum = totalSum + i;       
+    }
+    return totalSum;
 };
 
 // Do not edit below this line
